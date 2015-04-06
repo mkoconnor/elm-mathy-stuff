df8f72c7dc8fd71d51d86d181461c0adeb3e274a
Michael O'Connor <mkoconnor@gmail.com>
2015-04-06 09:32:45 -0400

    some work

--- Polygon.elm
+++ Polygon.elm
@@ -1,3 +1,5 @@
+import Window
+import Text
 import List
 import Graphics.Element as E
 import Graphics.Collage as C
@@ -18,4 +20,8 @@ ngon args =
   let elements = List.map (\((x,y), element) -> 
     C.move (args.outerRadius * x, args.outerRadius * y) (C.toForm element)) withCoords
   in
-  C.outlined (C.solid Color.blue) shape :: elements
+  C.filled Color.lightBlue shape :: elements
+
+thisNgon = ngon { elements = List.map Text.asText [1,2,4,5,7,8,11], ngonRadius=100,outerRadius=110 }
+
+main = Signal.map (\(width, height) -> C.collage width height thisNgon) Window.dimensions
