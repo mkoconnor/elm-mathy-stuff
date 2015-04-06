8abf020a9d6248aba759bcc4758f0aafeaa462ad
Michael O'Connor <mkoconnor@gmail.com>
2015-04-06 16:19:45 -0400

    polygon work

--- Polygon.elm
+++ Polygon.elm
@@ -30,6 +30,11 @@ initialize seed =
   in
   { indices = Array.fromList vertices', seed = seed'' }
 
+mod : Int -> Int -> Int
+mod i mod = 
+  let ans = i % mod in
+  if ans == mod then 0 else ans
+
 updateModel : Update -> Model -> Model
 updateModel update model = 
   case update of
@@ -38,12 +43,12 @@ updateModel update model =
      case Array.get i model.indices of
        Nothing -> model
        Just x  ->
-         if i <= 0
+         if x > 0
          then model
          else
            let 
-              iPlusOne = (i + 1) % Array.length model.indices
-              iMinusOne = ((i - 1) % Array.length model.indices)
+              iPlusOne = mod (i + 1) (Array.length model.indices)
+              iMinusOne = mod (i - 1) (Array.length model.indices)
            in            
            case Array.get iPlusOne model.indices of
               Nothing -> model
@@ -58,7 +63,7 @@ updateModel update model =
 updates : Signal.Channel Update
 updates = Signal.channel Regenerate
 
-toElement : Model -> {width:Int, height:Int} -> List C.Form
+toElement : Model -> {width:Int, height:Int} -> E.Element
 toElement model dims = 
   let maxdim = toFloat (min dims.width dims.height) / 2 in
   let ngonRadius = maxdim * 0.8 in
@@ -75,9 +80,10 @@ toElement model dims =
   let elements = List.map (\((x,y), element) -> 
     C.move (outerRadius * x, outerRadius * y) (C.toForm element)) withCoords
   in
-  C.filled Color.lightBlue shape :: elements
+  let button = I.button (Signal.send updates Regenerate) "Regenerate Polygon" in
+  C.collage dims.width dims.height (C.filled Color.lightBlue shape :: C.toForm button :: elements)
 
 models : Signal Model
 models = Utils.foldp updateModel (Signal.map initialize Utils.seedFromInitialTime) (Signal.subscribe updates)
 
-main = Signal.map2 (\(width, height) model -> C.collage width height (toElement model {width = width, height = height })) Window.dimensions models
+main = Signal.map2 (\(width, height) model -> toElement model {width = width, height = height }) Window.dimensions models
