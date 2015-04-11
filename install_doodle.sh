88c63f1ab2a9a0c55b7c1acc08324ec65fe5477f
Michael O'Connor <mkoconnor@gmail.com>
2015-04-11 16:15:42 -0400

    fixes

--- install_doodle.sh
+++ install_doodle.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 elm-make Doodle.elm --output doodle.js
-scp Doodle.elm doodle.js doodle.html birdglue:public_html/doodle
+scp Doodle.elm doodle.js birdglue:public_html/doodle
+scp doodle.html birdglue:public_html/doodle/index.html
